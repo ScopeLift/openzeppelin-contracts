a1a84821cf66d1b331cbd065e0ecac587234631d
David Laprade <david.laprade@gmail.com>
2022-03-28 16:58:54 -0400

    Roll our own uint85's

--- contracts/governance/extensions/GovernorCountingFractional.sol
+++ contracts/governance/extensions/GovernorCountingFractional.sol
@@ -28,7 +28,10 @@ abstract contract GovernorCountingFractional is Governor {
         uint80 abstainVotes;
     }
 
-    mapping(uint256 => ProposalVote) private _proposalVotes;
+    // proposal id --> encoded votes
+    mapping(uint256 => uint256) private _proposalVotes;
+
+    // proposal id --> (address --> whether the address voted)
     mapping(uint256 => mapping(address => bool)) private _proposalVotersHasVoted;
 
     /**
@@ -67,11 +70,20 @@ abstract contract GovernorCountingFractional is Governor {
             uint256 abstainVotes
         )
     {
-        ProposalVote storage proposalvote = _proposalVotes[proposalId];
+        uint256 _forVotes;
+        uint256 _againstVotes;
+        uint256 _abstainVotes;
+
+        (
+          _forVotes,
+          _againstVotes,
+          _abstainVotes
+        ) = _decodeVotes(_proposalVotes[proposalId]);
+
         return (
-          uint256(proposalvote.againstVotes) * _votePrecision(),
-          uint256(proposalvote.forVotes) * _votePrecision(),
-          uint256(proposalvote.abstainVotes) * _votePrecision()
+          _againstVotes * _votePrecision(),
+          _forVotes * _votePrecision(),
+          _abstainVotes * _votePrecision()
         );
     }
 
@@ -79,18 +91,34 @@ abstract contract GovernorCountingFractional is Governor {
      * @dev See {Governor-_quorumReached}.
      */
     function _quorumReached(uint256 proposalId) internal view virtual override returns (bool) {
-        ProposalVote storage proposalvote = _proposalVotes[proposalId];
+        uint256 _forVotes;
+        uint256 _abstainVotes;
 
-        return quorum(proposalSnapshot(proposalId)) <= proposalvote.forVotes + proposalvote.abstainVotes;
+        (
+          _forVotes,
+          , // _againstVotes
+          _abstainVotes
+        ) = _decodeVotes(_proposalVotes[proposalId]);
+
+        return quorum(proposalSnapshot(proposalId)) <= (
+          _forVotes + _abstainVotes
+        ) * _votePrecision();
     }
 
     /**
      * @dev See {Governor-_voteSucceeded}. In this module, the forVotes must be strictly over the againstVotes.
      */
     function _voteSucceeded(uint256 proposalId) internal view virtual override returns (bool) {
-        ProposalVote storage proposalvote = _proposalVotes[proposalId];
+        uint256 _forVotes;
+        uint256 _againstVotes;
 
-        return proposalvote.forVotes > proposalvote.againstVotes;
+        (
+          _forVotes,
+          _againstVotes
+          , // _abstainVotes
+        ) = _decodeVotes(_proposalVotes[proposalId]);
+
+        return _forVotes > _againstVotes;
     }
 
     /**
@@ -144,14 +172,54 @@ abstract contract GovernorCountingFractional is Governor {
             abstainVotes = SafeCast.toUint80(_abstainVotes / _votePrecision());
         }
 
-        ProposalVote memory existingProposalVote = _proposalVotes[proposalId];
+        uint256 existingProposalVote = _proposalVotes[proposalId];
 
-        ProposalVote memory _proposalvote = ProposalVote(
-          existingProposalVote.againstVotes + againstVotes,
-          existingProposalVote.forVotes + forVotes,
-          existingProposalVote.abstainVotes + abstainVotes
+        uint256 _existingForVotes;
+        uint256 _existingAgainstVotes;
+        uint256 _existingAbstainVotes;
+
+        (
+          _existingForVotes,
+          _existingAgainstVotes,
+          _existingAbstainVotes
+        ) = _decodeVotes(existingProposalVote);
+
+        _proposalVotes[proposalId] = _encodeVotes(
+          _existingForVotes + forVotes,
+          _existingAgainstVotes + againstVotes,
+          _existingAbstainVotes + abstainVotes
         );
+    }
 
-        _proposalVotes[proposalId] = _proposalvote;
+    uint256 public constant BITS = 85;
+    uint256 public constant MAX_UINT85 = 2 ** BITS - 1;
+    uint256 public constant MASK = 0x1fffffffffffffffffffff;
+
+    function _encodeVotes(
+      uint256 _for,
+      uint256 _against,
+      uint256 _abstain
+    ) internal pure returns(uint256) {
+      require(_for <= MAX_UINT85, "too many for votes");
+      require(_against <= MAX_UINT85, "too many against votes");
+      require(_abstain <= MAX_UINT85, "too many abstain votes");
+
+      // Shift by BITS to move the value to the correct position.
+      // x << y is equivalent to x * 2 ** y.
+      uint256 _shiftedFor = _for;
+      uint256 _shiftedAgainst = _against << BITS;
+      uint256 _shiftedAbstain = _abstain << BITS * 2;
+
+      return _shiftedAbstain | _shiftedAgainst | _shiftedFor;
+    }
+
+    function _decodeVotes(uint256 encodedVotes) internal pure returns (
+      uint256 _for,
+      uint256 _against,
+      uint256 _abstain
+    ) {
+      _for     = MASK & encodedVotes;
+      _against = MASK & encodedVotes >> BITS;
+      _abstain = MASK & encodedVotes >> BITS * 2;
     }
 }
